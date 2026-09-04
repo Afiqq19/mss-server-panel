594249e43eff3b9bd334987a41ae024625ef8eec
MSyafiq19 <mhdsyafiqsyahmi19@gmail.com>
2026-09-04 11:45:40 +0700

    build(frontend): compile flutter web bundle and update public directory

--- mss-backend/public/flutter_service_worker.js
+++ mss-backend/public/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ad5a38f5bf8348d607844c770ff4c85a
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "14f620401b0dc165a1729dbf62f68494",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "38dd768607a4e5aeac9489d49289c564",
+"flutter_bootstrap.js": "ebf157eb404e59db88a8391e5568cc50",
 "icons/Icon-192.png": "203e8f1976f669ce7ad37a44ec4b9c2a",
 "icons/Icon-512.png": "b7b9ca1967ab45032ef00adae067db5d",
 "icons/Icon-maskable-192.png": "203e8f1976f669ce7ad37a44ec4b9c2a",
